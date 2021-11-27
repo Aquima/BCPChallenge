8b092218f449627937776f2b6dcb5f2ab939fb6b
Jordy Aguilar <55517744+houaguilar@users.noreply.github.com>
2021-11-27 14:32:39 -0500

    Initial Commit

---
+++ BCPChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BCPChallenge
+//
+//  Created by Jordy Aguilar on 27/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
